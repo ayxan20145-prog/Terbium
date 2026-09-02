9da4596991714a78a9c5f2fb1b9b92abfa708c2f
ayxan <ayxan.20145@gmail.com>
2026-09-02 21:41:25 +0400

    addded a next_token method for generating tokens

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq)]
 enum Token {
     Let,
     Name(String),
@@ -24,10 +25,92 @@ impl Lexer {
     fn advance(&mut self) {
         self.position += 1;
     }
+    fn next_token(&mut self) -> Token {
+        loop {
+            match self.current() {
+                Some(c) => {
+                    if c.is_whitespace() {
+                        self.advance();
+                    } else {
+                        break;
+                    }
+                }
+                None => break,
+            }
+        }
+
+        match self.current() {
+            None => Token::Eof,
+
+            Some('=') => {
+                self.advance();
+                Token::Equals
+            }
+
+            Some(';') => {
+                self.advance();
+                Token::Semicolon
+            }
+
+            Some(c) if c.is_ascii_digit() => {
+                let mut number = String::new();
+
+                loop {
+                    match self.current() {
+                        Some(c) => {
+                            if c.is_ascii_digit() {
+                                number.push(c);
+                                self.advance();
+                            } else {
+                                break;
+                            }
+                        }
+                        None => break,
+                    }
+                }
+
+                Token::Value(number.parse().unwrap())
+            }
+
+            Some(c) if c.is_alphabetic() => {
+                let mut name = String::new();
+
+                loop {
+                    match self.current() {
+                        Some(c) => {
+                            if c.is_alphanumeric() {
+                                name.push(c);
+                                self.advance();
+                            } else {
+                                break;
+                            }
+                        }
+                        None => break,
+                    }
+                }
+
+                if name == "let" {
+                    Token::Let
+                } else {
+                    Token::Name(name)
+                }
+            }
+
+            Some(c) => {
+                panic!("unexpected char: {}", c);
+            }
+        }
+    }
 }
 fn main() {
     let mut lexer = Lexer::new("let x = 5;");
-    println!("{:?}", lexer.current());
-    lexer.advance();
-    println!("{:?}", lexer.current());
+
+    loop {
+        let token = lexer.next_token();
+        println!("{:?}", token);
+
+        if token == Token::Eof {
+            break;
+        }
+    }
 }
